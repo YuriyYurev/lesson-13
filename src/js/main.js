85cd4137a6999c7644c40cceaa0dc414e2aaaad5
YuriyYurev <iuriev-iurii1@mail.ru>
2019-06-01 14:35:04 +0700

    Написал скрипт через setinterval в событии при клике на кнопку

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,22 @@
-/* var button = document.querySelector('#button');
+var button = document.querySelector('#button');
 // присваиваю переменной button конекретный элемент из html
 var modal = document.querySelector('#modal');
 var buttonClose = document.querySelector('#close');
 
 button.addEventListener('click', function(){
   modal.classList.add('modal_active');
-}); */
+  setInterval(function time() {
+    modal.classList.remove('modal_active');
+  }, 5000);
+  clearInterval(function time(){}, 5000);
+});
+
+
 /* вешаю на переменную событие 'click'=клик мыши, далее функция,
 в которой происходит действие, в котором add=добавляет класс modal_active*/
-/* buttonClose.addEventListener('click', function(){
+buttonClose.addEventListener('click', function(){
   modal.classList.remove('modal_active');
-}); */
+
+});
 /* происходит событие 'click', в функции происзодит действие,
 тоесть при клике remove=убирает класс */
